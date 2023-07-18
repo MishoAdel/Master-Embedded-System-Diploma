ee51e608ed6c75c95359dcd7427794ae0226b700
MishoAdel <122019058+MishoAdel@users.noreply.github.com>
2023-07-18 14:31:13 +0300

    Delete Uunit_3_Embedded_C/Lesson 3 directory

--- Uunit_3_Embedded_C/Lesson 3/Lab1_Cont/app.c
+++
@@ -1,6 +0,0 @@
-#include "uart.h"
-unsigned char string[100]="Learn-In-Depth:<Michel>";
-
-void main(void){
-	UART0_String(string);
-}
--- Uunit_3_Embedded_C/Lesson 3/Lab1_Cont/uart.h
+++
@@ -1,6 +0,0 @@
-#ifndef UART_H
-#define UART_H
-
-void UART0_String (unsigned char* str);
-
-#endif
